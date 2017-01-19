bc3acaed5e25e73522492b9ff827e17e458869af
Ken Takahashi <takaken@redimpulz.com>
2017-01-20 04:30:19 +0900

    ミスを修正

--- scripts/get_radio_files.py
+++ scripts/get_radio_files.py
@@ -10,20 +10,20 @@ import json
 def download():
     times = 30
     
-    # # cdn = "origin/"
+    cdn = "origin/"
     # cdn = "cdn_azure/"
     # cdn = "cdn_cloudfront/"
-    cdn = "cdn_cloudflare/"
+    # cdn = "cdn_cloudflare/"
     
-    path = "files_2m/"
-    # path = "files_10m/"
+    # path = "files_2m/"
+    path = "files_10m/"
     # path = "files_20m/"
     # path = "files_50m/"
     
-    # url = "http://ichikawa-lab-exp.westus.cloudapp.azure.com/" + path
+    url = "http://ichikawa-lab-exp.westus.cloudapp.azure.com/" + path
     # url = "http://ichikawa-lab-exp1.azureedge.net/" + path
     # url = "http://dv12b46anbdab.cloudfront.net/" + path
-    url = "http://ichikawa-lab-exp.tkhskn.me/" + path
+    # url = "http://ichikawa-lab-exp.tkhskn.me/" + path
     result = []
     
     for count in range(1, times+1):
@@ -33,7 +33,7 @@ def download():
         for num in range(50):
             start = time.time()
             file_name = "{0}-{1}-{2}".format(count, sec, sec + num)
-            urllib.urlretrieve(url + str(sec), "./" + cdn + path + file_name)
+            urllib.urlretrieve(url + str(sec + num), "./" + cdn + path + file_name)
             elapsed_time = time.time() - start
             print ("elapsed_time:{0}".format(elapsed_time)) + "[sec]"
             each_result.update({file_name: elapsed_time})
