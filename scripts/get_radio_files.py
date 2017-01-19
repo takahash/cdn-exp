f6a99f749e48448e9ed752b34bf444d7ebba4579
Ubuntu <takaken@ichikawa-lab-exp-jp.s33sav1q2lwepltyhr3inhyzbh.mx.internal.cloudapp.net>
2017-01-19 17:16:26 +0000

    ファイルを修正

--- scripts/get_radio_files.py
+++ scripts/get_radio_files.py
@@ -8,24 +8,26 @@ import time
 import json
 
 def download():
-    times = 5
+    times = 30
+    # cdn = "cdn_azure/"
+    cdn = "origin/"
     path = "files_2m/"
     url = "http://ichikawa-lab-exp.westus.cloudapp.azure.com/" + path
     result = []
     
-    for count in range(1, times):
+    for count in range(1, times+1):
         group_dl_start = time.time()
         sec = random.randint(1, 450)
         each_result = {}
-        for num in range(5):
+        for num in range(50):
             start = time.time()
             file_name = "{0}-{1}-{2}".format(count, sec, sec + num)
-            urllib.urlretrieve(url + str(sec), "./" + path + file_name)
+            urllib.urlretrieve(url + str(sec), "./" + cdn + path + file_name)
             elapsed_time = time.time() - start
             print ("elapsed_time:{0}".format(elapsed_time)) + "[sec]"
             each_result.update({file_name: elapsed_time})
         result.append(each_result)
-    f = open("output.json", "w")
+    f = open("./{0}{1}output.json".format(cdn, path), "w")
     json.dump(result, f)
 
 if __name__ == "__main__":
