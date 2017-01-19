53acdf1f6e3cd0352449e5068a0c3dd33538d0c7
Ken Takahashi <takaken@redimpulz.com>
2017-01-20 05:51:18 +0900

    汎用的なスクリプトに修正

--- scripts/get_radio_files.py
+++ scripts/get_radio_files.py
@@ -8,32 +8,40 @@ import time
 import json
 
 def download():
-    times = 100
+    size = 2                # セグメントファイルのサイズ(MB)
+    sec_size = size / 2
+    times = 50              # 試行回数
+    period = 20
+    
+    path = "files_2m/"
+    # path = "files_4m/"
+    # path = "files_10m/"
+    # path = "files_20m/"
     
     cdn = "origin/"
     # cdn = "cdn_azure/"
     # cdn = "cdn_cloudfront/"
     # cdn = "cdn_cloudflare/"
     
-    # path = "files_2m/"
-    path = "files_10m/"
-    # path = "files_20m/"
-    # path = "files_50m/"
-    
     url = "http://ichikawa-lab-exp.westus.cloudapp.azure.com/" + path
     # url = "http://ichikawa-lab-exp1.azureedge.net/" + path
     # url = "http://dv12b46anbdab.cloudfront.net/" + path
     # url = "http://ichikawa-lab-exp.tkhskn.me/" + path
     result = []
-    
+
     for count in range(1, times+1):
-        group_dl_start = time.time()
         sec = random.randint(1, 450)
+        if (sec % sec_size) == 0:
+            start_file = sec
+            end_file = sec + (period / sec_size)
+        else:
+            start_file = (sec / sec_size) + 1
+            end_file = start_file + ((period / sec_size) + 1)
         each_result = {}
-        for num in range(20):
+        for num in range(start_file, end_file + 1):
             start = time.time()
-            file_name = "{0}-{1}-{2}".format(count, sec, sec + num)
-            urllib.urlretrieve(url + str(sec + num), "./" + cdn + path + file_name)
+            file_name = "{0}-{1}-{2}".format(count, sec, num)
+            urllib.urlretrieve(url + str(num), "./{0}{1}{2}".format(cdn, path, file_name))
             elapsed_time = time.time() - start
             print ("elapsed_time:{0}".format(elapsed_time)) + "[sec]"
             each_result.update({file_name: elapsed_time})
@@ -43,4 +51,3 @@ def download():
 
 if __name__ == "__main__":
     download()
- 
