4f72644bc7c85518a389897160415b5073302c36
Ubuntu <takaken@ichikawa-lab-exp-jp.s33sav1q2lwepltyhr3inhyzbh.mx.internal.cloudapp.net>
2017-01-29 09:04:53 +0000

    shを追加

--- scripts/cdn-exp.sh
+++ scripts/cdn-exp.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-python get_radio_files_thread.py 2 0
-python get_radio_files_thread.py 4 0
-python get_radio_files_thread.py 10 0
-python get_radio_files_thread.py 2 1
-python get_radio_files_thread.py 4 1
-python get_radio_files_thread.py 10 1
+# python get_radio_files_thread.py 2 0
+# python get_radio_files_thread.py 4 0
+# python get_radio_files_thread.py 10 0
+# python get_radio_files_thread.py 2 1
+# python get_radio_files_thread.py 4 1
+# python get_radio_files_thread.py 4 1
+python get_radio_files_thread.py 6 0
+python get_radio_files_thread.py 8 0
+python get_radio_files_thread.py 6 1
+python get_radio_files_thread.py 8 1
